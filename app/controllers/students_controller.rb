78a8ef24fdc8864a2a2d7bb63d1834a1d1b6c5e0
Jeremy Parker <jrobinparker@gmail.com>
2018-06-19 21:48:02 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   
   def activate
     if @student.active = false 
-      @student.update_attribute(:active, false)
+      @student.update_attribute(:active)
     else 
       @student.update_attribute(:active, false)
     end
